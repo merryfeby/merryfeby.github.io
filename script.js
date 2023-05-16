5004fd74a953874c6a65e872e01a0adddda1ee98
Rinko7 <merryfebyanti@gmail.com>
2023-05-16 20:22:34 +0700

    IWWW Final Project

---
+++ script.js
@@ -0,0 +1,53 @@
+  var btnvar1 = document.getElementById('btn1');
+  function Toggle1(){
+    if (btnvar1.style.color =="red") {
+      btnvar1.style.color = "grey"
+    } else {
+      btnvar1.style.color = "red"
+    }
+  }
+
+  var btnvar2 = document.getElementById('btn2');
+  function Toggle2(){
+    if (btnvar2.style.color =="red") {
+      btnvar2.style.color = "grey"
+    } else {
+      btnvar2.style.color = "red"
+    }
+  }
+
+  var btnvar3 = document.getElementById('btn3');
+  function Toggle3(){
+    if (btnvar3.style.color =="red") {
+      btnvar3.style.color = "grey"
+    } else {
+      btnvar3.style.color = "red"
+    }
+  }
+
+  var btnvar4 = document.getElementById('btn4');
+  function Toggle4(){
+    if (btnvar4.style.color =="red") {
+      btnvar4.style.color = "grey"
+    } else {
+      btnvar4.style.color = "red"
+    }
+  }
+
+  var btnvar5 = document.getElementById('btn5');
+  function Toggle5(){
+    if (btnvar5.style.color =="red") {
+      btnvar5.style.color = "grey"
+    } else {
+      btnvar5.style.color = "red"
+    }
+  }
+
+  var btnvar6 = document.getElementById('btn6');
+  function Toggle6(){
+    if (btnvar6.style.color =="red") {
+      btnvar6.style.color = "grey"
+    } else {
+      btnvar6.style.color = "red"
+    }
+  }
